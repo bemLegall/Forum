0505545592094bf1fceab3d2dc6f7058181f5bc0
Thiago Thomazini <tt.thomazini@gmail.com>
2022-01-10 21:28:15 -0300

    implementacao da paginacao, sistema de login e cache

---
+++ src/main/java/br/com/alura/forum/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package br.com.alura.forum;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration 
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	
+	
+	private AutenticacaoService autenticacaoService;
+	
+	public SecurityConfiguration(AutenticacaoService autenticacaoService) {
+		this.autenticacaoService = autenticacaoService;
+	}
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+
+		return super.authenticationManager();
+	}
+	
+	//configuracoes de autenticacao
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+		
+		
+	}
+	
+	//configuracoes de autorizacao
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"/topicos").permitAll()
+		.antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+		.antMatchers(HttpMethod.POST,"/auth").permitAll()
+		.anyRequest().authenticated()
+		.and().csrf().disable() 
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	
+	}
+	//configuracoes de recursos estaticos (js,css, imagens, etc).
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
+
+	/*
+	 * public static void main(String[] args) { System.out.println(new
+	 * BCryptPasswordEncoder().encode("123")); }
+	 */
+	
+	
+}
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,8 +1,9 @@
 package br.com.alura.forum.repository;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import br.com.alura.forum.modelo.Topico;
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable page);
 
 	
 
